1d2930880620d2e9da023105960fe39cb934bb5a
snackoncode <ahmed.alkarboly@gmail.com>
2023-01-06 14:42:15 -0700

    Update IPFS_Registry.sol

--- IPFS_Registry.sol
+++ IPFS_Registry.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.7.0;
 
 contract IPFS_Registry {
 
+    //Deployed Contract: 0xf5C5A7429fb1716ED9c112b1537416D03737D0C9
+
     // Mapping from content id to content struct
     mapping(address => string) public contents;
 
