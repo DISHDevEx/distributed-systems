d833db7e46331fa9b6228e4648721d389fc2d497
snackoncode <ahmed.alkarboly@gmail.com>
2023-01-06 14:34:31 -0700

    New solidity

---
+++ IPFS_Registry.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+contract IPFS_Registry {
+
+    // Mapping from content id to content struct
+    mapping(address => string) public contents;
+
+    function setFilePublic(string memory file) public {
+        contents[msg.sender] = file;
+    }
+
+    function setFileExternal(string memory file) external {
+        contents[msg.sender] = file;
+    }
+
+}
